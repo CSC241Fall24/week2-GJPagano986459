77509617a6dd3ada8ae627fa6ca93c1d811a2297
GJPagano986459 <GP986459@wcupa.edu>
2024-09-09 16:04:19 -0400

    Completed Week 2 assignments

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -14,10 +14,20 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
-        if (s.length() == 1) {
-            return s;
+        if (s == null || s.length() == 0) {
+            return s; // Return as is
         }
-        return reverse(s.substring(1)) + s.charAt(0);
+        // Call the recursive helper function
+        return reverseHelper(s, 0);
+    }
+
+    private static String reverseHelper(String s, int index) {
+        // Base case: when the index reaches the end of the string
+        if (index == s.length() - 1) {
+            return String.valueOf(s.charAt(index));
+        }
+        // Recursive call: reverse the rest of the string and append the current character
+        return reverseHelper(s, index + 1) + s.charAt(index);
         // Hint: The base case is when the string length is 1
         // Hint: Use substring to get the rest of the string
         // Hint: Use charAt to get the first character

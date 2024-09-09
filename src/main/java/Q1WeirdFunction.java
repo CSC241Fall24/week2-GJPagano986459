30690752fd26fcaa6873d28ae3271b1f53932394
GJPagano986459 <GP986459@wcupa.edu>
2024-09-09 15:53:26 -0400

    Completed Week 2 assignments

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -1,14 +1,36 @@
 // src/main/java/Q1WeirdFunction.java
+// Greg Pagano
+// 9/9/2024
+// Dr. Chen
+// Dr. Chen created a ”weird” function f(n), based on the following rules:
+// 1). If n < 3, then f(n) = n
+// 2). If n ≥ 3, then f(n) = f(n − 1) + 2f(n − 2) + 3f(n − 3).
+// • Please create a recursive method to implement this function f(n)
+// • Please create an iterative method to implement this function.
+
 
 public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
         // TODO: Implement the recursive function
-        return 0;
+        if (n < 3) {
+            return n;
+        }
+        return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
     }
 
     public static int fIterative(int n) {
         // TODO: Implement the iterative function
-        return 0;
+        if (n < 3) {
+            return n;
+        }
+        int a = 0, b = 1, c = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = a + 2 * b + 3 * c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
     }
 }
--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -1,9 +1,25 @@
 // src/main/java/Q4ReverseString.java
-
+/*
+ * Reverse String Using Recursion. Given the following class structure:
+public class Q4ReverseString {
+public static void main(String[] args) {
+System.out.println(reverse("Hello"));
+}
+public static String reverse(String s) {
+// TODO: Implement the recursive function to reverse the string
+}
+}
+ */
 public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
-        return "";
+        if (s.length() == 1) {
+            return s;
+        }
+        return reverse(s.substring(1)) + s.charAt(0);
+        // Hint: The base case is when the string length is 1
+        // Hint: Use substring to get the rest of the string
+        // Hint: Use charAt to get the first character
     }
 }
